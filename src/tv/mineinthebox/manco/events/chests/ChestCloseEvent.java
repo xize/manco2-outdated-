6a53554eaa204c4737a75387632a345470a3c5af
xize <glucassen@live.nl>
2015-02-07 18:15:11 +0100

    removed memory removal, as CratePlayer should do this

--- src/tv/mineinthebox/manco/events/chests/ChestCloseEvent.java
+++ src/tv/mineinthebox/manco/events/chests/ChestCloseEvent.java
@@ -1,12 +1,12 @@
 package tv.mineinthebox.manco.events.chests;
 
-import org.bukkit.Material;
 import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
 import tv.mineinthebox.manco.ManCo;
+import tv.mineinthebox.manco.instances.CratePlayer;
 
 public class ChestCloseEvent implements Listener {
 	
@@ -21,10 +21,8 @@ public class ChestCloseEvent implements Listener {
 		if(e.getInventory().getHolder() instanceof Chest) {
 			Chest chest = (Chest) e.getInventory().getHolder();
 			if(chest.hasMetadata("crate_owner")) {
-				chest.removeMetadata("crate_owner", pl);
-				chest.getBlock().setType(Material.AIR);
-				pl.getCrateOwners().remove(e.getPlayer().getName());
-				e.getPlayer().removeMetadata("crate", pl);
+				CratePlayer player = pl.getCratePlayer(e.getPlayer().getName());
+				player.remove();
 			}
 		}
 	}
