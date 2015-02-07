cf7fdd9a42f158d549346b25099a5c1cec4df89c
xize <glucassen@live.nl>
2015-02-07 14:38:13 +0100

    remove unwanted hooks as our custom call BlockPlaceEvent creates mass compatability

--- src/tv/mineinthebox/manco/hooks/Hook.java
+++ src/tv/mineinthebox/manco/hooks/Hook.java
@@ -5,27 +5,20 @@ import org.bukkit.Bukkit;
 public class Hook {
 	
 	public boolean isVaultEnabled() {
-		return Bukkit.getPluginManager().isPluginEnabled("Vault");
+		try {
+			Class<?> vault = Class.forName("net.milkbowl.vault.economy.Economy");
+			if(Bukkit.getServicesManager().getRegistration(vault) != null) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch(Exception e) {
+			return false;
+		}
 	}
 	
 	public boolean isWorldGuardEnabled() {
 		return Bukkit.getPluginManager().isPluginEnabled("WorldGuard");
-	}
-	
-	public boolean isWorldEditEnabled() {
-		return Bukkit.getPluginManager().isPluginEnabled("WorldEdit");
-	}
-	
-	public boolean isCoreProtectEnabled() {
-		return Bukkit.getPluginManager().isPluginEnabled("CoreProtect");
-	}
-	
-	public boolean isLogBlockEnabled() {
-		return Bukkit.getPluginManager().isPluginEnabled("LogBlock");
-	}
-	
-	public boolean isPrismEnabled() {
-		return Bukkit.getPluginManager().isPluginEnabled("Prism");
 	}
 	
 	public boolean isNCPEnabled() {
